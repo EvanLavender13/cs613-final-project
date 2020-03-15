44156a34ed7b574efb301ee5f6c38a4dc3d785f0
Evan <evanlavender13@gmail.com>
2020-03-14 21:00:14 -0400

    Initial commit

---
+++ algorithms.py
@@ -0,0 +1,355 @@
+from abc import abstractmethod
+
+import numpy as np
+
+from tqdm import tqdm
+
+from scipy.stats import multivariate_normal
+from sklearn.base import BaseEstimator, RegressorMixin, ClassifierMixin
+from sklearn.linear_model.base import LinearModel, LinearClassifierMixin
+from sklearn.utils.validation import check_X_y, check_array, check_random_state
+
+class EvolutionStrategy(BaseEstimator):
+
+    def __init__(self, objective=None, n_features=None, n_pop=100, n_iter=100, random_state=None):
+        self.objective = objective
+        self.n_features = n_features
+        self.n_pop = n_pop
+        self.n_iter = n_iter
+        self.random_state = random_state
+        self.classifier = False
+
+    def fit(self, X, y):
+        X, y = check_X_y(X, y)
+        self.objective.set_model(self, X, y)
+
+        self.random_ = check_random_state(self.random_state)
+        if not self.n_features:
+            self.n_features = 1 + X.shape[1]
+        self.history_ = np.zeros(self.n_iter)
+
+        self.mu_ = np.zeros(self.n_features)
+        self.intercept_ = self.mu_[0]
+        self.coef_ = np.array([self.mu_[1:]])
+
+        if self.classifier:
+            self.classes_ = np.unique(y)
+
+        self._pre_iterate()
+        for g in tqdm(range(self.n_iter)):
+            pop = self._sample_population()
+            fit = np.fromiter(map(self.objective.evaluate, pop), np.float)
+            self._update(pop, fit)
+
+            self.history_[g] = fit[np.argmax(fit)]
+            self.intercept_ = self.mu_[0]
+            self.coef_ = np.array([self.mu_[1:]])
+
+        return self
+
+    def _pre_iterate(self):
+        pass
+
+    @abstractmethod
+    def _sample_population(self):
+        pass
+
+    @abstractmethod
+    def _update(self, pop, fit):
+        pass
+
+class SimpleES(EvolutionStrategy):
+    def __init__(self, objective=None, n_features=None, n_pop=100, n_iter=100, random_state=None, sigma=0.2):
+        self.objective = objective
+        self.n_features = n_features
+        self.n_pop = n_pop
+        self.n_iter = n_iter
+        self.random_state = random_state
+        self.sigma = sigma
+
+    def _sample_population(self):
+        return self.mu_ + self.sigma * self.random_.randn(self.n_pop, self.n_features)
+
+    def _update(self, pop, fit):
+        best = pop[np.argmax(fit)]
+        self.mu_ = best
+
+class SimpleESRegressor(SimpleES, LinearModel, RegressorMixin):
+
+    def __init__(self, objective=None, n_features=None, n_pop=100, n_iter=100, random_state=None, sigma=0.2):
+        self.objective = objective
+        self.n_features = n_features
+        self.n_pop = n_pop
+        self.n_iter = n_iter
+        self.random_state = random_state
+        self.sigma = sigma
+        self.classifier = False
+
+class SimpleESClassifier(SimpleES, LinearClassifierMixin):
+
+    def __init__(self, objective=None, n_features=None, n_pop=100, n_iter=100, random_state=None, sigma=0.2):
+        self.objective = objective
+        self.n_features = n_features
+        self.n_pop = n_pop
+        self.n_iter = n_iter
+        self.random_state = random_state
+        self.sigma = sigma
+        self.classifier = True
+
+
+class GeneticES(EvolutionStrategy):
+
+    def __init__(self, objective=None, n_features=None, n_pop=100, n_iter=100, random_state=None, sigma=0.2, n_elite=10):
+        self.objective = objective
+        self.n_features = n_features
+        self.n_pop = n_pop
+        self.n_iter = n_iter
+        self.random_state = random_state
+        self.sigma = sigma
+        self.n_elite = n_elite
+        self.classifier = True
+
+    def _pre_iterate(self):
+        pop = self.mu_ + self.sigma * self.random_.randn(self.n_pop, self.n_features)
+        fit = np.fromiter(map(self.objective.evaluate, pop), np.float)
+        elite_indices = np.argsort(fit)[-np.int(self.n_elite):]
+        self.elite_ = pop[elite_indices]
+
+    def _sample_population(self):
+        pop = self.sigma * self.random_.randn(self.n_pop, self.n_features)
+        for i in range(self.n_pop):
+            index_a = self.random_.choice(self.n_elite)
+            index_b = self.random_.choice(self.n_elite)
+            
+            a = self.elite_[index_a]
+            b = self.elite_[index_b]
+            c = np.copy(a)
+            indices = np.where(self.random_.rand(self.n_features) > 0.5)
+            c[indices] = b[indices]
+            pop[i] += c
+
+        return pop
+
+    def _update(self, pop, fit):
+        elite_indices = np.argsort(fit)[-np.int(self.n_elite):]
+        self.elite_ = pop[elite_indices]
+
+        self.mu_ = np.mean(self.elite_, axis=0) 
+
+class GeneticESRegressor(GeneticES, LinearModel, RegressorMixin):
+
+    def __init__(self, objective=None, n_features=None, n_pop=100, n_iter=100, random_state=None, sigma=0.2, n_elite=10):
+        self.objective = objective
+        self.n_features = n_features
+        self.n_pop = n_pop
+        self.n_iter = n_iter
+        self.random_state = random_state
+        self.sigma = sigma
+        self.n_elite = n_elite
+        self.classifier = False
+
+class GeneticESClassifier(GeneticES, LinearClassifierMixin):
+
+    def __init__(self, objective=None, n_features=None, n_pop=100, n_iter=100, random_state=None, sigma=0.2, n_elite=10):
+        self.objective = objective
+        self.n_features = n_features
+        self.n_pop = n_pop
+        self.n_iter = n_iter
+        self.random_state = random_state
+        self.sigma = sigma
+        self.n_elite = n_elite
+        self.classifier = True
+
+
+class NaturalES(EvolutionStrategy):
+
+    def __init__(self, objective=None, n_features=None, n_pop=100, n_iter=100, random_state=None, sigma=0.2, alpha=0.1):
+        self.objective = objective
+        self.n_features = n_features
+        self.n_pop = n_pop
+        self.n_iter = n_iter
+        self.random_state = random_state
+        self.sigma = sigma
+        self.alpha = alpha
+
+    def _sample_population(self):
+        self.noise_ = self.random_.randn(self.n_pop, self.n_features)
+        pop = self.mu_ + self.sigma * self.noise_
+        return pop
+
+    def _update(self, pop, fit):
+        fit_n = (fit - np.mean(fit)) / np.std(fit, ddof=1)
+        self.mu_ += (self.alpha / (self.n_pop * self.sigma) * np.dot(self.noise_.T, fit_n))
+
+class NaturalESRegressor(NaturalES, LinearModel, RegressorMixin):
+
+    def __init__(self, objective=None, n_features=None, n_pop=100, n_iter=100, random_state=None, sigma=0.2, alpha=0.1):
+        self.objective = objective
+        self.n_features = n_features
+        self.n_pop = n_pop
+        self.n_iter = n_iter
+        self.random_state = random_state
+        self.sigma = sigma
+        self.alpha = alpha
+        self.classifier = False
+
+class NaturalESClassifier(NaturalES, LinearClassifierMixin):
+
+    def __init__(self, objective=None, n_features=None, n_pop=100, n_iter=100, random_state=None, sigma=0.2, alpha=0.1):
+        self.objective = objective
+        self.n_features = n_features
+        self.n_pop = n_pop
+        self.n_iter = n_iter
+        self.random_state = random_state
+        self.sigma = sigma
+        self.alpha = alpha
+        self.classifier = True
+
+
+class CMAES(BaseEstimator, RegressorMixin):
+
+    def __init__(self, objective=None, n_features=None, pop_size=100, iterations=100, sigma=0.2, num_elite=10):
+        self.objective = objective
+        self.n_features = n_features
+        self.pop_size = pop_size
+        self.iterations = iterations
+        self.sigma = sigma
+        self.num_elite = num_elite
+
+    def fit(self, X, y):
+        X, y = check_X_y(X, y)
+
+        self.n_ = X.shape[1]
+        self.mu_ = np.zeros(self.n_)
+
+        self.history_ = np.zeros(self.iterations)
+
+        self.cov_ = np.identity(self.n_)
+        self.mvn_ = multivariate_normal(mean=self.mu_, cov=self.cov_)
+
+        zero = np.zeros(self.n_)
+        for g in range(self.iterations):
+            # sample
+            pop = self.mu_ + self.sigma * np.random.multivariate_normal(zero, self.cov_, self.pop_size)
+
+            # evaluate
+            fit = np.fromiter(map(self.objective.evaluate, pop), np.float)
+
+            # update
+            elite_indices = np.argsort(fit)[-np.int(self.num_elite):]
+            elite_pop = pop[elite_indices]
+            # print(elite_pop)
+            old_mu = self.mu_
+            self.mu_ = np.mean(elite_pop, axis=0)
+            # print("mu", self.mu_)
+
+            elite_pop -= old_mu
+            self.cov_ = np.dot(elite_pop.T, elite_pop.conj()) / (self.num_elite - 1)
+            # print(self.cov_)
+            # print()
+
+            best_index = np.argmax(fit)
+            self.history_[g] = fit[best_index]
+
+        self.theta_ = self.mu_
+
+        return self
+
+    def predict(self, X):
+        X = check_array(X)
+
+        return np.dot(X, self.theta_)
+
+class DifferentialEvolution(BaseEstimator):
+
+    def __init__(self, objective=None, n_features=None, n_pop=10, n_iter=100, random_state=None, bounds=None, F=0.8, cx_pb=0.6):
+        self.objective = objective
+        self.n_features = n_features
+        self.n_pop = n_pop
+        self.n_iter = n_iter
+        self.random_state = random_state
+        self.bounds = bounds
+        self.F = F
+        self.cx_pb = cx_pb
+        self.classifier = False
+
+    def fit(self, X, y):
+        X, y = check_X_y(X, y)
+        self.objective.set_model(self, X, y)
+
+        self.random_ = check_random_state(self.random_state)
+        if not self.n_features:
+            self.n_features = 1 + X.shape[1]
+        self.history_ = np.zeros(self.n_iter)
+
+        self.mu_ = np.zeros(self.n_features)
+        self.intercept_ = self.mu_[0]
+        self.coef_ = np.array([self.mu_[1:]])
+
+        if self.classifier:
+            self.classes_ = np.unique(y)
+
+        min_bound, max_bound = self.bounds.T
+        pop = self.random_.uniform(min_bound, max_bound, (self.n_pop, self.n_features))
+        fit = np.fromiter(map(self.objective.evaluate, pop), np.float)
+
+        for g in tqdm(range(self.n_iter)):
+
+            for i in range(self.n_pop):
+                # choose random vectors
+                r_i = [j for j in range(self.n_pop) if j != i]
+                x1, x2, x3 = pop[self.random_.choice(r_i, 3, replace=False)]
+
+                # create trial vector
+                trial = x1 + self.F * (x2 - x3)
+
+                # determine crossover points
+                n = self.random_.randint(0, self.n_features - 1)
+                L = 1
+
+                while self.random_.rand() < self.cx_pb and L < self.n_features:
+                    L += 1
+                
+                v = np.arange(n, n + L - 1) % self.n_features
+                u = np.copy(pop[i])
+                u[v] = trial[v]
+
+                f = self.objective.evaluate(u)
+                if f > fit[i]:
+                    fit[i] = f
+                    pop[i] = u
+
+            best_index = np.argmax(fit)
+
+            self.history_[g] = fit[best_index]
+            self.mu_ = pop[best_index]
+            self.intercept_ = self.mu_[0]
+            self.coef_ = np.array([self.mu_[1:]])
+
+        return self
+
+class DifferentialEvolutionRegressor(DifferentialEvolution, LinearModel, RegressorMixin):
+
+    def __init__(self, objective=None, n_features=None, n_pop=10, n_iter=100, random_state=None, bounds=None, F=0.8, cx_pb=0.6):
+        self.objective = objective
+        self.n_features = n_features
+        self.n_pop = n_pop
+        self.n_iter = n_iter
+        self.random_state = random_state
+        self.bounds = bounds
+        self.F = F
+        self.cx_pb = cx_pb
+        self.classifier = False
+
+class DifferentialEvolutionClassifier(DifferentialEvolution, LinearClassifierMixin):
+
+    def __init__(self, objective=None, n_features=None, n_pop=10, n_iter=100, random_state=None, bounds=None, F=0.8, cx_pb=0.6):
+        self.objective = objective
+        self.n_features = n_features
+        self.n_pop = n_pop
+        self.n_iter = n_iter
+        self.random_state = random_state
+        self.bounds = bounds
+        self.F = F
+        self.cx_pb = cx_pb
+        self.classifier = True
---
+++ objectives.py
@@ -0,0 +1,62 @@
+import numpy as np
+
+from abc import abstractmethod
+
+eps = 1e-200
+
+class Objective(object):
+
+    def set_model(self, model, X, y):
+        self.model = model
+        self.X = X
+        self.y = y
+
+    @abstractmethod
+    def evaluate(self, w):
+        pass
+
+class RSSObjective(Objective):
+
+    def evaluate(self, w):
+        y_i = w[0] + np.dot(self.X, w[1:])
+        error = np.linalg.norm(self.y - y_i, ord=2)
+        return -error
+
+class ScoreObjective(Objective):
+
+    def evaluate(self, w):
+        self.model.intercept_ = w[0]
+        self.model.coef_ = np.array([w[1:]])
+        return self.model.score(self.X, self.y)
+
+class RidgeObjective(Objective):
+
+    def __init__(self, alpha=1.0):
+        self.alpha = alpha
+
+    def evaluate(self, w):
+        y_i = w[0] + np.dot(self.X, w[1:])
+        penalty = self.alpha * np.linalg.norm(w, ord=2)
+        error = np.linalg.norm(self.y - y_i, ord=2) + penalty
+        return -error
+
+class CrossEntropyObjective(Objective):
+
+    def sigmoid(self, y_i):
+        return 1.0 / (1 + np.exp(-y_i))
+
+    def evaluate(self, w):
+        y_i = self.sigmoid(w[0] + np.dot(self.X, w[1:]))
+        error = np.sum(self.y * np.log(y_i + eps) + (1 - self.y) * np.log((1 - y_i) + eps))
+        return error
+
+class MultiCrossEntropyObjective(Objective):
+
+    def softmax(self, y_i):
+        e_y = np.exp(y_i - np.max(y_i))
+        return e_y / np.sum(e_y)
+
+    def evaluate(self, w):
+        y_i = self.softmax(w[0] + np.dot(self.X, w[1:]))
+        error = np.sum(self.y * np.log(y_i + eps))
+        return error
